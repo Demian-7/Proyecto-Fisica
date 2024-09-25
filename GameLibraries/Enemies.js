3b015a82b6df97a632d59f9556058f6317c75254
Revvan <santi.sanchezcorallo@gmail.com>
2024-09-25 15:58:11 -0300

    Class Enemy

--- GameLibraries/Enemies.js
+++ GameLibraries/Enemies.js
@@ -1,26 +1,67 @@
 class GroundEnemy extends GameObject {
 
-  constructor(posX, posY, size, speed) {
+  constructor(type, posX, posY, size, speed, score) {
     super();
     this.name = "Ground Enemy"
-    this.colliderShape = ColliderShape.SQUARE;
+    this.type = type;  // Set the enemy type ('ground' or 'flying')
+
     this.pos = createVector(posX, posY);
     this.w = size;
     this.h = size;
+    this.size = size;
     this.speed = speed;
+    this.angle = 0; // For flying enemies
+    this.amplitude = random(20, 50); // For sine wave movement
+
+    // Speed multiplier logic
+    let speedMultiplier = 1 + score / 120; // Adjusting speed based on score
+    let maxSpeedMultiplier = 3;
+    this.speed = min(speed * speedMultiplier, speed * maxSpeedMultiplier); // Adjusting speed
+    
+    // Position based on type
+    if (type === 'ground') {
+      this.isCircle = false; // Ground enemies are always squares
+      this.colliderShape = ColliderShape.SQUARE;
+      this.pos.y = HEIGHT - this.size / 2;
+      //this.pos.x = -400 * speedMultiplier; // Increase speed
+    } else if (type === 'flying') {
+      this.isCircle = true; // Flying enemies are always circles
+      this.colliderShape = ColliderShape.CIRCLE;
+      this.pos.y = random(HEIGHT / 2, HEIGHT - this.size * 2);
+      //this.pos.x = -200 * speedMultiplier; // Increase speed
+      this.angle = 0;
+      this.amplitude = random(20, 50);
+      this.angularSpeed = random(1, 3); // radians per second
+    }
   }
 
   SetSpeed(speed) {
-    this.speed = speed;
+    this.speed = speed; 
+  }
+  offscreen() {
+    return this.pos.x < -this.size;
   }
 
   Render() {
+    stroke(255); // Stroke color
+    strokeWeight(2); // Stroke weight
     fill(220, 40, 50);
-    rect(this.pos.x, this.pos.y, this.w, this.h);
+
+    if (this.isCircle) {
+      ellipse(this.pos.x, this.pos.y, this.size * 2);  // Draw a circle
+    } else {
+      rectMode(CENTER);
+      rect(this.pos.x, this.pos.y, this.size * 2);  // Draw a square
+    }
   }
 
   Update(dt) {
     this.pos.x -= this.speed*dt;
+    // If flying enemy (using sine wave motion)
+    if (this.type === 'flying' && this.isCircle) {
+      this.angle += random(1, 3) * dt;
+      this.pos.y += sin(this.angle) * this.amplitude * dt;  // Vertical movement using sine
+    }
   }
 
   Collide(other){
@@ -28,6 +69,32 @@ class GroundEnemy extends GameObject {
   }
 
   CheckCollition(other){
-
+    
   }
 }
+
+    /*
+    if (other.isCircle) {
+      // Circle-Rectangle Collision logic
+      let testX = other.pos.x;
+      let testY = other.pos.y;
+
+      if (other.pos.x < this.pos.x) testX = this.pos.x;
+      else if (other.pos.x > this.pos.x + this.size) testX = this.pos.x + this.size;
+      if (other.pos.y < this.pos.y) testY = this.pos.y;
+      else if (other.pos.y > this.pos.y + this.size) testY = this.pos.y + this.size;
+
+      let distX = other.pos.x - testX;
+      let distY = other.pos.y - testY;
+      let distance = sqrt(distX * distX + distY * distY);
+
+      return distance <= other.size / 2;
+    } else {
+      // Rectangle-Rectangle Collision
+      return !(
+        other.pos.x + other.size / 2 < this.pos.x ||
+        other.pos.x - other.size / 2 > this.pos.x + this.size ||
+        other.pos.y + other.size / 2 < this.pos.y ||
+        other.pos.y - other.size / 2 > this.pos.y + this.size
+      );
+    }*/ 
--- sketch.js
+++ sketch.js
@@ -11,8 +11,12 @@ function setup() {
   gameController = new GameController();
   
   player = new Player(100, 100, 40);
- // theFloor = new TheFloor();
-  enemy = new GroundEnemy(WIDTH-100, HEIGHT-30, 20, 10);
+  // theFloor = new TheFloor();
+  enemy = new GroundEnemy(random(['ground', 'flying']), WIDTH - 100, HEIGHT - 10, 20, 100, 100);  // Ground enemy (square)
+  enemy = new GroundEnemy(random(['ground', 'flying']), WIDTH - 100, HEIGHT - 10, 20, 100, 100);  // Ground enemy (square)
+  enemy = new GroundEnemy(random(['ground', 'flying']), WIDTH - 100, HEIGHT - 10, 20, 100, 100);  // Ground enemy (square)
+  enemy = new GroundEnemy(random(['ground', 'flying']), WIDTH - 100, HEIGHT - 10, 20, 100, 100);  // Ground enemy (square)
+  enemy = new GroundEnemy(random(['ground', 'flying']), WIDTH - 100, HEIGHT - 10, 20, 100, 100);  // Ground enemy (square)
   for(let i=0; i<3; i++){
   clouds.push(new CloudLayer(i + 1));
   }
